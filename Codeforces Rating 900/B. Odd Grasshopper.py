7186fc8f39d7555a986e6ea3f0e551badb7fa2b6
Clear20-22 <sojib1472004@gmail.com>
2025-08-05 00:21:59 +0600

    update

--- Codeforces Rating 900/B. Odd Grasshopper.py
+++ Codeforces Rating 900/B. Odd Grasshopper.py
@@ -1,39 +1,13 @@
 # Problem: https://codeforces.com/problemset/problem/1607/B
-# Solution: # Problem: https://codeforces.com/problemset/problem/1607/B
-# Solution: 
+# Solution: https://codeforces.com/contest/1607/submission/332407937
 
 for _ in range(int(input())):
     n, m = map(int, input().split())
 
-    if m < 4:
-        for i in range(1, m+1):
-            if n % 2 == 0:
-                n -= i
-            else:
-                n += i
-    else:
-        for i in range(m//4 * 4 + 1, m+1):
-            if n % 2 == 0:
-                n -= i
-            else:
-                n += i
-
-    print(n)
-
-for _ in range(int(input())):
-    n, m = map(int, input().split())
-
-    if m < 4:
-        for i in range(1, m+1):
-            if n % 2 == 0:
-                n -= i
-            else:
-                n += i
-    else:
-        for i in range(m//4 * 4 + 1, m+1):
-            if n % 2 == 0:
-                n -= i
-            else:
-                n += i
+    for i in range(m//4 * 4 + 1, m+1):
+        if n % 2 == 0:
+            n -= i
+        else:
+            n += i
 
     print(n)
