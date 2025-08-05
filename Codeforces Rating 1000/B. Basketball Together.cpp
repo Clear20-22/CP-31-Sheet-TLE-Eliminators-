9b8d6d5c5539218184352d8d054b2d0c43106e39
Clear20-22 <sojib1472004@gmail.com>
2025-08-05 14:17:17 +0600

    add 1000

---
+++ Codeforces Rating 1000/B. Basketball Together.cpp
@@ -0,0 +1,69 @@
+// Problem: https://codeforces.com/problemset/problem/1725/B
+// Solution: https://codeforces.com/contest/1725/submission/332467924
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <limits>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <sstream>
+#include <bitset>
+#include <deque>
+#include <list>
+#include <cassert>
+#include <random>
+#include <functional>
+#include <tuple>
+using namespace std;
+
+#define Bismillah ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define Multiple_Test int t; cin >> t; while (t--) solve();
+#define Single_Test solve();
+#define Code_End return 0;
+#define endl '\n'
+#define int long long
+#define MOD 1000000007
+#define INF numeric_limits<int>::max()
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort(a.rbegin(), a.rend());
+    int ans = 0;
+    int player = 0;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        int power = m / a[i] + 1;
+        if(power + player <= n){
+            ans ++;
+            player += power;
+            
+        }
+    }
+
+    cout << ans << endl;
+
+}
+
+int32_t main()
+{
+    Bismillah
+    // Multiple_Test
+    Single_Test
+    Code_End
+}
